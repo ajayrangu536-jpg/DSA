e6dea39d2fa344b20635029b69e2c36e93cb4f75
ajayrangu536-jpg <ajayrangu536@gmail.com>
2026-04-05 12:26:00 +0530

    Time: 32 ms (18.17%) | Memory: 114.9 MB (5.01%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+     HashSet <Integer> set = new HashSet <>();
+     int duplicate =0;
+     for(int i=0;i<nums.length;i++){
+        if(set.contains(nums[i])){
+            duplicate=nums[i];
+        }
+        set.add(nums[i]);
+        }
+ 
+    return duplicate; 
+    }
+    }
