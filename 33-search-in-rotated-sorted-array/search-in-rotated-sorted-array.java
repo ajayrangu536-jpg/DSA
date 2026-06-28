2639f0c6bb0e8b7215cc22c0c893857125c34138
Ajay Rangu <ajayrangu536@gmail.com>
2026-06-29 00:33:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (46.12%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,26 +1,39 @@
 class Solution {
     public int search(int[] nums, int target) {
-       /* int left =0;
+        int left =0;
         int right =nums.length-1;
-        while(left<right){
+        while(left<=right){
             int mid = left+(right-left)/2;
+
+
             if(nums[mid]==target){
                 return mid;
 
             }
-            else if(nums[mid]>target){
-                right=mid+1;
-
+        if(nums[left]<=nums[mid]){
+            if(nums[left]<=target&& target<=nums[mid]){
+                right=mid-1;
             }
             else{
-                left=mid-1;
+                left=mid+1;
             }
         }
-        return left;
-
+         else {
+            if(nums[mid]<=target&&target<=nums[right]){
+            left =  mid+1;
+         }
+         else{
+            right=mid-1;
+         }
         
+
+         }
     }
-    */
+    return -1;
+    }
+}
+    /*
+  Linear serach ----- > 0(N);
     for(int i=0;i<nums.length;i++){
         if(nums[i]==target){
             return i;
@@ -29,3 +42,4 @@ class Solution {
     return -1;
 }
 }
+*/
