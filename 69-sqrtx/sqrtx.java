9ce181be10c2dcb371fada4e9485c4b681f93970
Ajay Rangu <ajayrangu536@gmail.com>
2026-07-02 23:22:49 +0530

    Time: 1 ms (97.64%) | Memory: 42.6 MB (61.22%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        long left =1;
+        long right =x;
+        while(left<=right){
+            long mid = left+(right-left)/2;
+            long sqr = mid*mid;
+            if(sqr==x){
+                return (int)mid;
+            }
+
+             else if(sqr<x){
+
+                left = mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return (int)right;
+    }
+}
