8639df0beb70a8921fc921c1e6a60ff91cfe885b
Ajay Rangu <ajayrangu536@gmail.com>
2026-07-06 20:20:28 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (46.09%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
+        /*for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
                 return true;
             }
@@ -8,4 +8,40 @@ class Solution {
         return false;
         
     }
+    */
+    int left =0;
+    int right = nums.length-1;
+    while(left<=right){
+        int mid = left+(right-left)/2;
+        //if(nums[left]<=nums[mid]){
+            if(nums[mid]==target){
+              
+                return true;
+            }
+            if(nums[left]==nums[mid]&& nums[mid]==nums[right]){
+                left++;
+                right--;
+                continue;
+            }
+
+         if(nums[left]<=nums[mid]){
+        if(nums[left]<=target&& target<=nums[mid]){
+            right = mid-1;
+        }
+        else{
+            left =mid+1;
+        }
+         }
+        else {
+            if(nums[mid]<=target&& target<=nums[right]){
+            left = mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+    }
+    }
+    return false;
+
+}
 }
