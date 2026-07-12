a3892b34a099797490ead083193c62af01078d12
Ajay Rangu <ajayrangu536@gmail.com>
2026-07-12 23:20:29 +0530

    Time: 3 ms (89.59%) | Memory: 48 MB (93.07%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n =matrix.length;
+        int m =matrix[0].length;
+        int row =0;
+        int column=m-1;
+        while(row<n&& column>=0){
+            if(matrix[row][column]==target){
+                return true;
+            }
+            else if(matrix[row][column]<target){
+                row++;
+            }
+            else{
+                column--;
+            }
+        }
+        return false;
+
+        }
+        
+    }
